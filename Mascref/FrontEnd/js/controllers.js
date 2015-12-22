76f9f9ced97f69d8f65be82490d667a436438476
Davi Candido <davivc@gmail.com>
2015-12-22 18:04:17 +1000

    Broken controllers in smaller pieces

--- Mascref/FrontEnd/js/controllers.js
+++ Mascref/FrontEnd/js/controllers.js
@@ -90,75 +90,7 @@ angular.module('app.controllers', ['pascalprecht.translate'])
 
       $scope.profile();
 
-  })
-  // Dashboard Controllers
-  .controller('DashboardCtrl', ['$scope', '$translate', '$state', function ($scope, $translate, $state) {
-      if (!$scope.authenticated) {
-        $state.go('access.signin');
-      }
-
-      
-        
-  }])
-  .controller('DashboardStatsCtrl', ['$scope', '$translate', '$filter', 'Dashboard', function ($scope, $translate, $filter, Dashboard) {
-      $scope.stats = {
-        'countries': 0,
-        'projects': 0,
-        'sites': 0,
-        'towns': 0,
-        'surveys': 0,
-        'transects': 0
-      }
-
-      $scope.researchers = []
-      $scope.totalAdmin = 0;
-      $scope.totalMembers = 0;
-      $scope.totalResearchers = 0;
-
-      $scope.getStats = function () {
-        Dashboard.stats()
-        .then(function (data) {
-          $scope.stats = data;
-        }, function (error) {
-          console.error('Dash Stats: ' + error);
-          $scope.stats.error = error;
-        });
-      }
-
-      $scope.getResearchers = function () {
-        Dashboard.researchers()
-        .then(function (data) {
-          $scope.researchers = data;
-        }, function (error) {
-          console.error('Dash Stats: ' + error);
-          $scope.researchers.error = error;
-        });
-      }   
-    
-      $scope.$watch('researchers', function (data) {
-        $scope.totalAdmin = $filter('filter')($scope.researchers, { role: 1 }, true).length;
-        $scope.totalMembers = $filter('filter')($scope.researchers, { role: 2 }, true).length;
-        $scope.totalResearchers = $filter('filter')($scope.researchers, { role: 3 } && { role: null } && { role: undefined }, true).length;
-      });
-
-      $scope.getStats();
-      $scope.getResearchers();
-    
-  }])
-  .controller('DashboardRecentCtrl', ['$scope', '$translate', function ($scope, $translate) {
-
-  }])  
-  .controller('ProjectViewSurveyCtrl', ['$scope', '$translate', '$stateParams', 'uiGmapGoogleMapApi', function ($scope, $translate, $stateParams, uiGmapGoogleMapApi) {
-    console.log($stateParams);
-    $scope.map = { center: { latitude: -18.20, longitude: 179 }, zoom: 7, options: { scrollwheel: false, panControl: false, streetViewControl: false } };
-
-    // uiGmapGoogleMapApi is a promise.
-    // The "then" callback function provides the google.maps object.
-    uiGmapGoogleMapApi.then(function (maps) {
-
-      $('.angular-google-map-container').css('height', '300px');
-    });
-  }])
+  }) 
   // Maps Controllers
   .controller("MapsCtrl", function($scope, uiGmapGoogleMapApi) {
     // Do stuff with your $scope.
